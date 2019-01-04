b9050251f1048819fea7778b9b621767e6c8880a
danijel@PROTECH <danijelcopic@gmail.com>
2019-01-04 17:49:54 +0100

    Commit 42

--- app/src/main/java/com/protech/testsymlink/MainActivity.java
+++ app/src/main/java/com/protech/testsymlink/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        Toast.makeText(this, "41 unos + nesto", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "42 unos + nesto", Toast.LENGTH_SHORT).show();
 
     }
 }
